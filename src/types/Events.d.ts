d74139497935c13b358eaed821fa2457f5b44e29
Muthu Kumar <muthukumar@thefeathers.in>
2020-08-09 21:55:25 +0530

    [cleanup] rm modules moved out of repo

--- src/types/Events.d.ts
+++
@@ -1,9 +0,0 @@
-type Event = [string, string];
-type Listener = (ctx: any) => void;
-
-export type Events = {
-	on: (event: Event, listener: Listener) => Events;
-	off: (event: Event, listener: Listener) => Events;
-	once: (event: Event, listener: Listener) => Events;
-	emit: (event: Event, ctx: any) => Events;
-};
