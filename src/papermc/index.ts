3c8a02006fa92a36a052c3fd7ca0417b6841a530
Muthu Kumar <muthukumar@thefeathers.in>
2020-08-18 20:32:43 +0530

    [feat] export Parser created by Factory

--- src/papermc/index.ts
+++ src/papermc/index.ts
@@ -1,14 +1,13 @@
 import Vanilla from "../vanilla";
 
-const P1162 = {
-	...Vanilla["1.16.2"],
+const P1162 = Vanilla["1.16.2"].extend({
 	timestamp: function () {
 		return "^\\[(?<time>\\d{2}:\\d{2}:\\d{2})";
 	},
 	loglevel: function () {
 		return "(?<loglevel>.*?)\\]:";
 	},
-};
+});
 
 const PaperMC = {
 	"1.16.2": P1162,
