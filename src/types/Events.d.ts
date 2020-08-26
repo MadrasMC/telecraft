6b59e400d8bdb233c5e84ea2dd3c1147c03bf7c8
Muthu Kumar <muthukumar@thefeathers.in>
2020-08-27 04:15:16 +0530

    [feat] replace Events with NodeJS EventEmitter

--- src/types/Events.d.ts
+++ src/types/Events.d.ts
@@ -1,8 +1,1 @@
-type Listener = (ctx: any) => void;
-
-export type Events = {
-	on: (event: string, listener: Listener) => Events;
-	off: (event: string, listener: Listener) => Events;
-	once: (event: string, listener: Listener) => Events;
-	emit: (event: string, ctx: any) => Events;
-};
+export type Events = NodeJS.EventEmitter;
