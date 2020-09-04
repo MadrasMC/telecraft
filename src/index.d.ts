8ff6d700c98e8a93c86ef7b67d179e051d8a60fb
Muthu Kumar <muthukumar@thefeathers.in>
2020-09-05 03:32:01 +0530

    [feat] better Plugin, Store and IO types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,6 @@
 export { Events } from "./types/Events";
 export { Parser } from "./types/Parser";
-export { Plugin } from "./types/Plugin";
 export { Server } from "./types/Server";
 export { Store } from "./types/Store";
+export { IO } from "./types/IO";
+export { Plugin } from "./types/Plugin";
