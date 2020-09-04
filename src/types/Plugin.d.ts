e964a9141f9b7ed8f301bccce667028db58f5f02
Muthu Kumar <muthukumar@thefeathers.in>
2020-09-05 03:32:01 +0530

    [feat] better Plugin, Store and IO types

--- src/types/Plugin.d.ts
+++ src/types/Plugin.d.ts
@@ -2,11 +2,20 @@ import { Events } from "./Events";
 import { Store } from "./Store";
 import { Server } from "./Server";
 
-export type Plugin<Opts = never, Deps extends any[] = [], Exports = any> = (
+export type Plugin<Opts = never, Deps extends any[] = any[], Exports = any> = (
 	opts: Opts,
 ) => {
 	name: string;
+	version: string;
 	dependencies?: string[];
 	exports?: Exports;
-	start: (events: Events, store: Store, server: Server, dep: Deps) => void;
+	start: (
+		props: {
+			events: Events;
+			store: Store;
+			server: Server;
+			console: Console;
+		},
+		dep: Deps,
+	) => void;
 };
