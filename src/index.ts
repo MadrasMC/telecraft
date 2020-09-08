2f9c10e0e05f71b87fa2f15287af2cdf745044e6
Muthu Kumar <muthukumar@thefeathers.in>
2020-09-08 19:36:32 +0530

    [feat] initial impl of leveldb store
    Signed-off-by: Muthu Kumar <muthukumar@thefeathers.in>

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import { Store } from "@telecraft/types";
+
+import fs from "fs";
+import path from "path";
+
+import levelup from "levelup";
+import leveldown from "leveldown";
+
+const StoreProvider = (location: string) => {
+	const stat = fs.statSync(location);
+
+	if (!stat.isDirectory()) throw new TypeError("No directory at " + location);
+
+	fs.accessSync(location, fs.constants.R_OK | fs.constants.W_OK);
+
+	return (name: string): Store => {
+		const store = levelup(leveldown(path.resolve(location, name)));
+
+		return {
+			get: key =>
+				store
+					.get(key)
+					// parse to object before returning
+					.then(value => JSON.parse(value.toString("utf-8"))),
+			set: (key, value) =>
+				store
+					// stringify to JSON before writing
+					.put(key, Buffer.from(JSON.stringify(value), "utf-8"))
+					.then(() => value),
+			remove: key => store.del(key),
+			clear: () => store.clear(),
+		};
+	};
+};
+
+export default StoreProvider;
