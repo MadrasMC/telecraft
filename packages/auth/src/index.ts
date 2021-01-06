fa62f7a5db2c682e8f6e28c43980437bfc58f4ad
Muthu Kumar <muthukumar@thefeathers.in>
2021-01-06 06:32:57 +0530

    [auth] safer rewrite wip

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,82 +1,156 @@
-import { Plugin } from "@telecraft/types";
+import { Events, Plugin } from "@telecraft/types";
+
+import { parse } from "nbt-ts";
+
+const pkg = require("../package.json") as { name: string; version: string };
+
+const createError = (...str: string[]) =>
+	new Error(`[${pkg.name}@${pkg.version}] ` + str.join(" "));
 
 // Get 4 random numbers
 const rand = () => String(Math.ceil(Math.random() * 10000));
 
-const gamemodes = ["survival", "creative", "adventure", "survival"];
+const gamemodes = ["adventure", "survival", "creative", "spectator"] as const;
+type gamemodes = typeof gamemodes[number];
+
+// const waitForUserData = (event: Events, user: string) => {
+// 	new Promise((res, rej) =>
+// 		event.once("minecraft:data", ctx => {
+// 			let data;
+// 			try {
+// 				data = parse(ctx.data);
+// 			} catch {
+// 				// Not the data we're expecting
+// 				return waitForUserData(event, user);
+// 			}
+
+// 			// if (data.)
+// 		}),
+// 	);
+// };
+
+type Messenger = {
+	send: (user: string, msg: string) => Promise<void>;
+	on: (type: string, content: string) => void;
+};
 
 const auth: Plugin<
-	{ enable: boolean },
-	{ telegram: { send: (user: string, msg: string) => void } }
-> = {
-	name: "telegram-auth",
-	plugin: ({ config = {}, dependencies: { telegram = null } = {} } = {}) => (
-		events,
-		store,
-		server,
-	) => {
+	{ enable: boolean; use: "@telecraft/telegram" | "@telecraft/irc" },
+	[Messenger]
+> = config => ({
+	name: pkg.name,
+	version: pkg.version,
+	dependencies: [config.use],
+	start: async ({ events, store, server, console }, [messenger]) => {
 		if (!config.enable) return;
-		if (!telegram)
-			throw new Error(
-				"[@telecraft/telegram-auth] Plugin was enabled, but dependency 'telegram' was not passed",
+		if (!messenger)
+			throw createError(
+				"Plugin was enabled, but dependency 'messenger' was not passed",
 			);
 
+		const authstore = await store();
+
+		const lock = (user: string) => {
+			server.send(`effect give ${user} minecraft:blindness 1000000`);
+			server.send(`effect give ${user} minecraft:slowness 1000000 255`);
+			server.send(`gamemode spectator ${user}`);
+			server.send(`deop ${user}`);
+		};
+
+		const unlock = (
+			user: string,
+			mode: typeof gamemodes[number],
+			isOp: boolean = false,
+		) => {
+			server.send(`effect clear ${user} minecraft:blindness 1000000`);
+			server.send(`effect clear ${user} minecraft:slowness 1000000 255`);
+			server.send(`gamemode ${mode} ${user}`);
+			if (isOp) server.send(`op ${user}`);
+		};
+
 		events.on("minecraft:join", async ctx => {
-			const tgUser = await store.get(["minecraft", ctx.user, "tgUser"]);
+			const tgUser = await authstore.get("mc:" + ctx.user);
 
-			server.send(`data get entity ${ctx.user} playerGameType`);
-			server.send(`data get entity ${ctx.user} Pos`);
-			server.send(`data get entity ${ctx.user} Dimension`);
-			server.send(`effect give ${ctx.user} minecraft:blindness 1000000`);
-			server.send(`effect give ${ctx.user} minecraft:slowness 1000000 255`);
-			server.send(`gamemode spectator ${ctx.user}`);
+			server.send(`data get entity ${ctx.user}`);
 
-			if (tgUser) {
-				telegram.send(tgUser, "Send /auth to authenticate yourself.");
-			} else {
-				server.send(`tellraw ${ctx.user} Send /link to the Telegram bot.`);
-			}
-		});
+			type Pos = [number, number, number];
 
-		events.on("telegram:link", async ctx => {
-			const token = rand();
-			const tgUser = ctx.user;
+			let playerGameType: gamemodes;
+			let pos: Pos;
+			let dimension: string;
 
-			await store.set(["telegram", token, "tgUser"], tgUser);
-			telegram.send(tgUser, `Send !link ${token} in Minecraft chat to link.`);
-		});
+			lock(ctx.user);
 
-		events.on("minecraft:message", async ctx => {
-			const [cmd, ...rest] = ctx.msg.split(" ");
-			if (cmd === "!link") {
-				const [token] = rest;
-				const tgUser = await store.get(["telegram", token, "tgUser"]);
-				if (token && tgUser) {
-					await store.set(["telegram", tgUser, "user"], ctx.user);
+			const interval = setInterval(() => {
+				pos &&
 					server.send(
-						`tellraw ${ctx.user} Successfully linked with Telegram user.`,
+						`execute in ${dimension} run tp ${ctx.user} ${pos.join(" ")}`,
 					);
-					telegram.send(
-						tgUser,
-						`Successfully linked with Minecraft player ${ctx.user}.`,
-					);
-				}
+			}, 400);
+
+			console.log(ctx.user);
+
+			events.once("minecraft:data", ctx => {
+				const data = parse(ctx.data) as any;
+
+				playerGameType = data.playerGameType;
+				pos = data.Pos as Pos;
+				dimension = data.Dimension;
+			});
+
+			if (typeof tgUser === "string") {
+				messenger.send(tgUser, "Send /auth to authenticate yourself.");
+			} else {
+				server.send(`tellraw ${ctx.user} "Send /link to the Telegram bot."`);
 			}
 		});
 
-		events.on("telegram:auth", async ctx => {
-			const tgUser = ctx.user;
+		// events.on(`${config.use}:link`, async ctx => {
+		// 	const token = rand();
+		// 	const tgUser = ctx.user;
 
-			const player = await store.get(["telegram", tgUser, "user"]);
+		// 	await authstore.set(["telegram", token, "tgUser"], tgUser);
+		// 	messenger.send(tgUser, `Send !link ${token} in Minecraft chat to link.`);
+		// });
 
-			if (!player) {
-				telegram.send(
-					tgUser,
-					`Not linked to a Minecraft player. Send /link first.`,
-				);
-			}
+		// events.on("minecraft:message", async ctx => {
+		// 	const [cmd, ...rest] = ctx.msg.split(" ");
+		// 	if (cmd === "!link") {
+		// 		const [token] = rest;
+		// 		const tgUser = await authstore.get(["telegram", token, "tgUser"]);
+		// 		if (token && tgUser) {
+		// 			await authstore.set(["telegram", tgUser, "user"], ctx.user);
+		// 			server.send(
+		// 				`tellraw ${ctx.user} "Successfully linked with Telegram user."`,
+		// 			);
+		// 			messenger.send(
+		// 				tgUser,
+		// 				`Successfully linked with Minecraft player ${ctx.user}.`,
+		// 			);
+		// 		}
+		// 	}
+		// });
+
+		// events.on(`${config.use}:auth`, async ctx => {
+		// 	const tgUser = ctx.user;
+
+		// 	const player = await authstore.get(["telegram", tgUser, "user"]);
+
+		// 	if (!player) {
+		// 		messenger.send(
+		// 			tgUser,
+		// 			`Not linked to a Minecraft player. Send /link first.`,
+		// 		);
+		// 	}
+
+		// 	// unlock(ctx.user, mode);
+		// });
+
+		events.on("core:close", () => {
+			// cleanup
+			authstore.close();
 		});
 	},
-};
+});
 
 export default auth;
