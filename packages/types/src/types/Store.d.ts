57ffdef09b2a7d6c872c57ef7f5be7670e39beeb
Muthu Kumar <muthukumar@thefeathers.in>
2021-01-13 01:05:10 +0530

    [feat:store] Store.find (wip)

--- packages/types/src/types/Store.d.ts
+++ packages/types/src/types/Store.d.ts
@@ -1,8 +1,9 @@
 type JSONable = string | number | boolean | any[] | object | null;
 
 export type Store = () => {
-	get: (key: string) => Promise<JSONable>;
+	get: (key: string) => Promise<JSONable | null>;
 	set: <Value extends JSONable>(key: string, value: Value) => Promise<Value>;
+	find: (key: string | RegExp) => Promise<string | null>;
 	remove: (key: string) => Promise<void>;
 	clear: () => Promise<void>;
 	close: () => Promise<void>;
