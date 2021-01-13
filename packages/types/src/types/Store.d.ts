797f654ab1b110231cc25bdf07f38f6739d0706a
Muthu Kumar <muthukumar@thefeathers.in>
2021-01-14 01:26:33 +0530

    [fix:store] return [k,v] on find, error tolerance
    Signed-off-by: Muthu Kumar <muthukumar@thefeathers.in>

--- packages/types/src/types/Store.d.ts
+++ packages/types/src/types/Store.d.ts
@@ -1,10 +1,10 @@
 type JSONable = string | number | boolean | any[] | object | null;
 
-export type Store = () => {
+export type Store = () => Promise<{
 	get: (key: string) => Promise<JSONable | null>;
 	set: <Value extends JSONable>(key: string, value: Value) => Promise<Value>;
-	find: (key: string | RegExp) => Promise<string | null>;
+	find: (key: string | RegExp) => Promise<[string, JSONable] | null>;
 	remove: (key: string) => Promise<void>;
 	clear: () => Promise<void>;
 	close: () => Promise<void>;
-};
+}>;
