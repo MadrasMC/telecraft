bbad6ca87c4a1307977fecf13be2600232eb0613
Muthu Kumar <muthukumar@thefeathers.in>
2021-01-14 02:40:53 +0530

    [feat:store] Store takes generic value type
    Signed-off-by: Muthu Kumar <muthukumar@thefeathers.in>

--- packages/types/src/types/Store.d.ts
+++ packages/types/src/types/Store.d.ts
@@ -1,9 +1,9 @@
 type JSONable = string | number | boolean | any[] | object | null;
 
-export type Store = () => Promise<{
-	get: (key: string) => Promise<JSONable | null>;
-	set: <Value extends JSONable>(key: string, value: Value) => Promise<Value>;
-	find: (key: string | RegExp) => Promise<[string, JSONable] | null>;
+export type Store = <J extends JSONable>() => Promise<{
+	get: (key: string) => Promise<J | null>;
+	set: <Value extends J>(key: string, value: Value) => Promise<Value>;
+	find: (key: string | RegExp) => Promise<[string, J] | null>;
 	remove: (key: string) => Promise<void>;
 	clear: () => Promise<void>;
 	close: () => Promise<void>;
