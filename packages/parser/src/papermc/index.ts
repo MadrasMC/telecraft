466f511afa7a5c605fa353aa7a581b26ae869d2d
Muthu Kumar <muthukumar@thefeathers.in>
2021-01-14 07:21:25 +0530

    [fix:parser] offline server join bug

--- packages/parser/src/papermc/index.ts
+++ packages/parser/src/papermc/index.ts
@@ -1,16 +1,17 @@
 import Vanilla from "../vanilla";
 
-const P1162 = Vanilla["1.16.2"].extend({
+const P116 = Vanilla["1.16"].extend({
 	timestamp: function () {
 		return "^\\[(?<time>\\d{2}:\\d{2}:\\d{2})";
 	},
 	loglevel: function () {
 		return "(?<loglevel>.*?)\\]:";
 	},
+	// Todo(mkr): verify whether vanilla join works on PaperMC
 });
 
 const PaperMC = {
-	"1.16.2": P1162,
+	"1.16": P116,
 };
 
 export default PaperMC;
