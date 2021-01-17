6bd64c9410c0867e5cacad3ba99480594b8bd957
Muthu Kumar <muthukumar@thefeathers.in>
2021-01-18 02:33:40 +0530

    [feat:store] Store::find takes a fn instead of str
    Signed-off-by: Muthu Kumar <muthukumar@thefeathers.in>

--- packages/types/src/types/Store.d.ts
+++ packages/types/src/types/Store.d.ts
@@ -3,7 +3,9 @@ type JSONable = string | number | boolean | any[] | object | null;
 export type Store = <J extends JSONable>() => Promise<{
 	get: (key: string) => Promise<J | null>;
 	set: <Value extends J>(key: string, value: Value) => Promise<Value>;
-	find: (key: string | RegExp) => Promise<[string, J] | null>;
+	find: <V extends J | null>(
+		query: (value: V) => boolean,
+	) => Promise<[string, V] | null>;
 	remove: (key: string) => Promise<void>;
 	clear: () => Promise<void>;
 	close: () => Promise<void>;
