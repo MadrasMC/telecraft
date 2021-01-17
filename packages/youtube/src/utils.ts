758c46d6ad823a302080bd9f11107a85de6b620a
Muthu Kumar <muthukumar@thefeathers.in>
2021-01-18 02:35:26 +0530

    [misc:yt] minor improvements
    Signed-off-by: Muthu Kumar <muthukumar@thefeathers.in>

--- packages/youtube/src/utils.ts
+++ packages/youtube/src/utils.ts
@@ -1,34 +1,29 @@
 export type MsgContext = {
-	text: string | ChatComponent[],
-	from: string
+	text: string | ChatComponent[];
+	from: string;
 };
 
 export type ChatComponent =
 	| {
 			text: string;
 			bold: boolean;
-			color?:
-				| "red"
-				| "white";
+			color?: "red" | "white";
 	  }
 	| string;
 
 export const MCChat = {
 	text: (text: string | ChatComponent[]): ChatComponent[] =>
 		typeof text === "string"
-			? text.replace(/\n/g, "\n\n ")
-					.split("\n")
-					.map(line => ({
-						text: line,
-						bold: false,
-					}))
+			? text.split("\n").map(line => ({
+					text: "\n " + line,
+					bold: false,
+			  }))
 			: text,
 
-	user: (name: string): ChatComponent[] =>
-		[
-			{ text: "YT", color: "red", bold: false },
-			{ text: ": " + name, color: "white", bold: false },
-		],
+	user: (name: string): ChatComponent[] => [
+		{ text: "YT", color: "red", bold: false },
+		{ text: ": " + name, color: "white", bold: false },
+	],
 
 	sender: (name: string): ChatComponent[] => [
 		{ text: "<", bold: true },
@@ -37,9 +32,7 @@ export const MCChat = {
 	],
 
 	message: (message: MsgContext): ChatComponent[] => [
-		...MCChat.sender(
-			message.from
-		),
+		...MCChat.sender(message.from),
 		...MCChat.text(message.text),
 	],
 };
