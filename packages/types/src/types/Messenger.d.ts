f4b67ba069b67c385c6b38dee5f1c6215695bb8b
Muthu Kumar <muthukumar@thefeathers.in>
2021-08-13 05:24:35 +0530

    feat: new Messenger type
    Signed-off-by: Muthu Kumar <muthukumar@thefeathers.in>

--- packages/types/src/types/Messenger.d.ts
+++ packages/types/src/types/Messenger.d.ts
@@ -1,13 +1,17 @@
-export type Listener = (context: any) => void;
+type Listener<Context> = (context: Context) => void;
 
-export type exports = {
-	send: (
-		type: "private" | "chat",
-		identifier: string | number,
-		msg: string,
-	) => Promise<void>;
-	on: (event: string, listener: Listener) => void;
-	once: (event: string, listener: Listener) => void;
-	off: (event: string, listener: Listener) => void;
-	cmdPrefix: string;
+export type Messenger<
+	Context,
+	Identifier extends string | number = string | number,
+	Type extends "private" | "chat" = "private" | "chat",
+> = {
+	listener: Listener<Context>;
+	emit: (ev: string, ctx: Context) => void;
+	exports: {
+		send: (type: Type, identifier: Identifier, msg: string) => Promise<void>;
+		on: (event: string, listener: Listener<Context>) => void;
+		once: (event: string, listener: Listener<Context>) => void;
+		off: (event: string, listener: Listener<Context>) => void;
+		cmdPrefix: string;
+	};
 };
