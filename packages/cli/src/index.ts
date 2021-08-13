db4fff03d7af90eb7962a9b1ee0b3c270daf48aa
Muthu Kumar <muthukumar@thefeathers.in>
2021-08-13 23:18:33 +0530

    feat: sample config
    Signed-off-by: Muthu Kumar <muthukumar@thefeathers.in>

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,43 +1,25 @@
-import https from "https";
-
 import core from "@telecraft/core";
 import { Vanilla } from "@telecraft/parser";
 import Telegram from "@telecraft/telegram";
 import Auth from "@telecraft/auth";
 import StoreProvider from "@telecraft/store";
-import YouTubeLive from "@telecraft/youtube";
 
-import { chatId, token, videoId, apiKey } from "./config";
+import { telegram, launch, storePath } from "./config.example";
 
 core({
 	config: {
-		launch: "/usr/bin/env java -Xmx3096M -Xms1024M -jar server.jar nogui",
-		cwd: "/media/Mizu/Minecraft/mkrcraft/server",
+		launch: launch.cmd,
+		cwd: launch.cwd,
 	},
 	parser: Vanilla["1.16"],
-	store: StoreProvider(
-		"/media/Mizu/Minecraft/mkrcraft/server/.telecraft/store",
-	),
+	store: StoreProvider(storePath),
 	plugins: [
 		Telegram({
-			token,
+			token: telegram.token,
 			enable: true,
-			chatId,
+			chatId: telegram.chatId,
 			list: { allow: true },
-			telegraf: {
-				telegram: {
-					agent: new https.Agent({
-						// @ts-ignore
-						family: 6,
-					}),
-				},
-			},
 		}),
 		Auth({ enable: true, use: "@telecraft/telegram" }),
-		YouTubeLive({
-			enable: true,
-			videoId,
-			apiKey,
-		}),
 	],
 });
