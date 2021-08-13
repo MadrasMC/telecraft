4ed3519e6dc39e99352e4acbefad6b22da90f9ad
Muthu Kumar <muthukumar@thefeathers.in>
2021-08-13 23:18:33 +0530

    feat: sample config
    Signed-off-by: Muthu Kumar <muthukumar@thefeathers.in>

---
+++ packages/cli/src/config.example.ts
@@ -0,0 +1,10 @@
+export const launch = {
+	cmd: "/usr/bin/env java -Xmx3096M -Xms1024M -jar server.jar nogui",
+	cwd: "/path/to/server",
+};
+export const storePath = "/path/to/server/.telecraft/store";
+
+export const telegram = {
+	token: "",
+	chatId: "",
+};
