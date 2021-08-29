a16a05f60d9dc1c3065b96f613f46bc5b6385e48
Muthu Kumar <muthukumar@thefeathers.in>
2021-08-29 07:55:03 +0530

    fix(irc): return early, minor fixes
    Signed-off-by: Muthu Kumar <muthukumar@thefeathers.in>

--- packages/irc/src/index.ts
+++ packages/irc/src/index.ts
@@ -14,7 +14,7 @@ type Opts = {
 	server: string;
 	nick: string;
 	channel: string;
-	clientOpts: irc.IClientOpts | undefined;
+	clientOpts?: irc.IClientOpts;
 };
 
 type IRCMessenger = Messenger<string, CtxBase, "chat">;
@@ -23,7 +23,7 @@ const IRC: Plugin<Opts, [], IRCMessenger["exports"]> = opts => {
 	const client = new irc.Client(opts.server, opts.nick, {
 		channels: [opts.channel],
 		...opts.clientOpts,
-		autoConnect: false
+		autoConnect: false,
 	});
 	const ev = new EventEmitter();
 
@@ -33,7 +33,7 @@ const IRC: Plugin<Opts, [], IRCMessenger["exports"]> = opts => {
 	const emit: IRCMessenger["emit"] = ev.emit.bind(ev);
 
 	const ircMessenger: IRCMessenger["exports"] = {
-		async send(channel: string, msg) {
+		async send(type, channel: string, msg) {
 			client.say(channel, msg);
 		},
 		on,
@@ -47,111 +47,107 @@ const IRC: Plugin<Opts, [], IRCMessenger["exports"]> = opts => {
 		version: pkg.version,
 		exports: ircMessenger,
 		start: async ({ events, server, console }, []) => {
-			if (opts.enable) {
-				let playersOnline = 0;
+			if (!opts.enable) return;
 
-				const send = (msg: string) => client.say(opts.channel, msg);
+			let playersOnline = 0;
 
-				const joinCallback = async () => {
-					const chan = client.channellist.find(x => x.name == opts.channel);
+			const send = (msg: string) => client.say(opts.channel, msg);
+
+			events.on("minecraft:started", () => {
+				client.addListener(`message${opts.channel}`, (from, message) => {
+					if (
+						// nobody online
+						playersOnline < 1 &&
+						// itsa me, bot
+						from === opts.nick
+					)
+						return;
+
+					if (isCommand(message)) {
+						const cmd = parseCommand(message);
+
+						const ctx = {
+							from: {
+								id: from,
+								source: opts.channel,
+								type: "chat" as const,
+							},
+							...cmd,
+						};
+
+						emit(cmd.cmd, ctx);
+					} else {
+						const chatMessage = MCChat.message({
+							from: from,
+							channel: opts.channel,
+							text: message,
+						});
+
+						server.send("tellraw @a " + JSON.stringify(chatMessage));
+					}
+				});
+
+				events.on("minecraft:message", ctx => {
+					send(code(ctx.user) + " " + escapeHTML(ctx.text));
+				});
+
+				events.on("minecraft:join", ctx => {
+					send(code(ctx.user + " joined the server"));
+					playersOnline += 1;
+				});
+
+				events.on("minecraft:leave", ctx => {
+					send(code(ctx.user + " left the server"));
+					playersOnline -= 1;
+				});
+
+				events.on("minecraft:self", ctx =>
+					send(code("* " + ctx.user + " " + ctx.text)),
+				);
+
+				events.on("minecraft:say", ctx =>
+					send(code(ctx.user + " says: " + ctx.text)),
+				);
+
+				events.on("minecraft:death", ctx =>
+					send(code(ctx.user + " " + ctx.text)),
+				);
+
+				events.on("minecraft:advancement", ctx =>
+					send(
+						code(ctx.user) +
+							" has made the advancement " +
+							code("[" + ctx.advancement + "]"),
+					),
+				);
+
+				events.on("minecraft:goal", ctx =>
+					send(
+						code(ctx.user) +
+							" has reached the goal " +
+							code("[" + ctx.goal + "]"),
+					),
+				);
+
+				events.on("minecraft:challenge", ctx =>
+					send(
+						code(ctx.user) +
+							" has completed the challenge " +
+							code("[" + ctx.challenge + "]"),
+					),
+				);
+			});
+
+			client.connect(3, () => {
+				client.join(opts.channel, async () => {
+					const chan = client.chans[opts.channel];
 
 					if (!chan)
-						throw createError(
-							"Fatal: Could not obtain requested channel",
-						);
+						throw createError("Fatal: Could not obtain requested channel");
 
-					console.log(`Bound to channel '${chan.name}'`);
-				}
-
-				events.on("minecraft:started", () => {
-					client.addListener(`message${opts.channel}`, (from, message) => {
-						if (
-							// nobody online
-							playersOnline < 1 &&
-							// itsa me, bot
-							from === opts.nick
-						)
-							return;
-
-						if (isCommand(message)) {
-							const cmd = parseCommand(message);
-
-							const ctx = {
-								from: {
-									id: from,
-									source: opts.channel,
-									type: "chat" as const,
-								},
-								...cmd,
-							};
-
-							emit(cmd.cmd, ctx);
-						} else {
-							const chatMessage = MCChat.message({
-								from: from,
-								channel: opts.channel,
-								text: message,
-							});
-
-							server.send("tellraw @a " + JSON.stringify(chatMessage));
-						}
-					});
-
-					events.on("minecraft:message", ctx => {
-						send(code(ctx.user) + " " + escapeHTML(ctx.text));
-					});
-
-					events.on("minecraft:join", ctx => {
-						send(code(ctx.user + " joined the server"));
-						playersOnline += 1;
-					});
-
-					events.on("minecraft:leave", ctx => {
-						send(code(ctx.user + " left the server"));
-						playersOnline -= 1;
-					});
-
-					events.on("minecraft:self", ctx =>
-						send(code("* " + ctx.user + " " + ctx.text)),
-					);
-
-					events.on("minecraft:say", ctx =>
-						send(code(ctx.user + " says: " + ctx.text)),
-					);
-
-					events.on("minecraft:death", ctx =>
-						send(code(ctx.user + " " + ctx.text)),
-					);
-
-					events.on("minecraft:advancement", ctx =>
-						send(
-							code(ctx.user) +
-								" has made the advancement " +
-								code("[" + ctx.advancement + "]"),
-						),
-					);
-
-					events.on("minecraft:goal", ctx =>
-						send(
-							code(ctx.user) +
-								" has reached the goal " +
-								code("[" + ctx.goal + "]"),
-						),
-					);
-
-					events.on("minecraft:challenge", ctx =>
-						send(
-							code(ctx.user) +
-								" has completed the challenge " +
-								code("[" + ctx.challenge + "]"),
-						),
-					);
+					console.log(`Bound to channel '${chan.key}'`);
 				});
-				
-				client.connect(3, () => {
-					client.join(opts.channel, () => joinCallback);
-				});
-			}
+			});
 		},
 	};
 };
